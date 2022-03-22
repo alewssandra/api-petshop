39f95e8f631db4d99ae4f4a0c3350843f49362e3
alewssandra <alewssandralima@gmail.com>
2022-03-22 17:30:47 -0300

    serializando fornecedore

--- api/Serializador.js
+++ api/Serializador.js
@@ -7,10 +7,38 @@ class Serializador {
 
     serializar (dados) {
         if (this.contentType === 'application/json') {
-            return this.json(dados)
+            //se o formato requisitado for json, retorna o json
+            return this.json(
+                this.filtrar(dados)
+            )
         }
 
         throw new ValorNaoSuportado(this.contentType);
+    }
+
+    filtrarObjeto (dados) {
+        const novoObjeto = {}
+
+        this.camposPublicos.forEach((campo) => {
+            //hasOwnPropety verifica se o objeto possui a propriedade
+            if(dados.hasOwnProperty(campo)) {
+                novoObjeto[campo] = dados[campo]
+            }
+        })
+
+        return novoObjeto
+    }
+
+    filtrar (dados) {
+        if (Array.isArray(dados)) {
+            dados = dados.map(item => {
+                return this.filtrarObjeto(item)
+            }) 
+        } else {
+            dados = this.filtrarObjeto(dados)
+        }
+ 
+        return dados
     }
 }
 
@@ -18,6 +46,11 @@ class SerializadorFornecedor extends Serializador {
     constructor (contentType) {
         super()
         this.contentType = contentType
+        this.camposPublicos = [
+            'id',
+            'empresa',
+            'categoria'
+        ]
     }
 }
 
