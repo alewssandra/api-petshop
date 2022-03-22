c20797e52f5e0205111ba3adc651ce5ba5ae8b77
alewssandra <alewssandralima@gmail.com>
2022-03-22 18:05:48 -0300

    campos extras no serializador

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -42,7 +42,8 @@ roteador.get('/:idFornecedor', async (requisicao, resposta) => {
         await fornecedor.carregar()
         resposta.status(200)
         const serializador = new SerializadorFornecedor(
-            resposta.getHeader('Content-Type')
+            resposta.getHeader('Content-Type'),
+            ['email', 'dataCriacao', 'dataAtualizacao', 'versao' ]
         )
         resposta.send(
             serializador.serializar(fornecedor)
