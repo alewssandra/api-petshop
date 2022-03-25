51bc38e1443c3fdfb293c36734b1b402738f287e
alewssandra <alewssandralima@gmail.com>
2022-03-25 16:55:01 -0300

    controllers

--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -1,4 +1,5 @@
 const Modelo = require('./ModeloTabelaProduto')
+const instancia = require('../../../banco-de-dados')
 
 //exporta a função que cria o modelo
 module.exports = {
@@ -45,5 +46,21 @@ module.exports = {
                 where: dadosDoProduto
             }
         )
+    },
+    subtrair (idProduto, idFornecedor, campo, quantidade) {
+        return instancia.transaction(async transacao => {
+            const produto = await Modelo.findOne({
+                where: {
+                    id: idProduto,
+                    fornecedor: idFornecedor
+                },
+            })
+
+            produto[campo] = quantidade
+
+            await produto.save()
+
+            return produto
+        })
     }
 }
