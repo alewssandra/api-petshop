2e82ca4fe0eb10469d6a03f257435737bf2cbb44
alewssandra <alewssandralima@gmail.com>
2022-03-25 16:28:16 -0300

    atualizando produtos

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -51,6 +51,32 @@ class Produto {
         this.dataAtualizacao = produto.dataAtualizacao
         this.versao = produto.versao
     }
+
+    atualizar () {
+        const dadosParaAtualizar = {}
+
+        if(typeof this.titulo === 'string' && this.titulo.length > 0) {
+            dadosParaAtualizar.titulo = this.titulo
+        }
+        if(typeof this.preco === 'number' && this.preco > 0) {
+            dadosParaAtualizar.preco = this.preco
+        }
+        if(typeof this.estoque === 'number' && this.estoque >= 0) {
+            dadosParaAtualizar.estoque = this.estoque
+        }
+        if(Object.keys(dadosParaAtualizar).length === 0) {
+            throw new Error('Nenhum dado para atualizar')
+        }
+
+        return Tabela.atualizar(
+            {
+                id: this.id, 
+                fornecedor: this.fornecedor
+            },
+            dadosParaAtualizar
+        )
+    }
+
 }
 
 module.exports = Produto
